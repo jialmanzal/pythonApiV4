326a0bb3c8b0cd8c390fb01e362a515c6d1818c1
jialmanzal <jesusalmanza@gmail.com>
2020-10-29 07:57:29 -0500

    Update client.py

--- service/client.py
+++ service/client.py
@@ -8,12 +8,12 @@ class Client():
         self.doc_id = doc_id
         self.preexistence = []
 
-    def add_preexistence(self, nPreexistence):
-        self.preexistence.append(nPreexistence)
+    def add_preexistence(self, n_preexistence):
+        self.preexistence.append(n_preexistence)
         return len(self.preexistence) - 1
 
-    def get_preexistence(self, pIndex):
-        if pIndex >= len(self.preexistence):
+    def get_preexistence(self, p_index):
+        if p_index >= len(self.preexistence):
             return 'There is no such preexistence'
         else:
             return self.preexistence[pIndex]
